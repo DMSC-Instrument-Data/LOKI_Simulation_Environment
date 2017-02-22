92f705e223ab6026f26bbd05bad01edc2c4e0bc9
LamarMoore <lamar.moore@stfc.ac.uk>
2017-02-22 09:06:28 +0000

    Last merge fixed

--- provision/pyqtgraph.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# Install pyqtgraph
-sudo pip install pyzmq
-sudo pip install mpi4py
-
-wget http://www.pyqtgraph.org/downloads/python-pyqtgraph_0.9.10-1_all.deb
-sudo dpkg -i python-pyqtgraph_0.9.10-1_all.deb
-
